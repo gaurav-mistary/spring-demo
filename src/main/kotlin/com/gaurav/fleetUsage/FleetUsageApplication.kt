465187482a882ce75d7dcecd22ab7d65b8ce5a8e
Gaurav Mistary <gm179gaurav@gmail.com>
2023-03-27 13:58:16 +0530

    Initial

---
+++ src/main/kotlin/com/gaurav/fleetUsage/FleetUsageApplication.kt
@@ -0,0 +1,11 @@
+package com.gaurav.fleetUsage
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FleetUsageApplication
+
+fun main(args: Array<String>) {
+	runApplication<FleetUsageApplication>(*args)
+}
---
+++ src/test/kotlin/com/gaurav/fleetUsage/FleetUsageApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.gaurav.fleetUsage
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FleetUsageApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
